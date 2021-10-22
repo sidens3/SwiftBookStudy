056238b0e9cc7ecac976f75ce34931e21e5fdad7
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2021-10-23 01:56:23 +0300

    add outlets and functions for changing view color by slider change

---
+++ 2.2/1/ColorSlider/ColorSlider/ColorSliderViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ColorSliderViewController.swift
+//  ColorSlider
+//
+//  Created by Михаил Зиновьев on 22.10.2021.
+//
+
+import UIKit
+
+enum Constants {
+    static let sliderValuerFormat = "%.2f"
+    static let defaultViewRadius: CGFloat = 8
+}
+
+class ColorSliderViewController: UIViewController {
+    @IBOutlet weak var currentColorView: UIView!
+    
+    @IBOutlet weak var redColorSaturationLabel: UILabel!
+    @IBOutlet weak var redColorSaturationSlider: UISlider!
+    
+    @IBOutlet weak var greenColorSaturationLabel: UILabel!
+    @IBOutlet weak var greenColorSaturationSlider: UISlider!
+    
+    @IBOutlet weak var blueColorSaturationLabel: UILabel!
+    @IBOutlet weak var blueColorSaturationSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentColorView.makeRound(with: Constants.defaultViewRadius)
+        
+        redColorSaturationSlider.defaultConfigure()
+        greenColorSaturationSlider.defaultConfigure()
+        blueColorSaturationSlider.defaultConfigure()
+        
+        redColorSaturationLabel.text = String(redColorSaturationSlider.value)
+        greenColorSaturationLabel.text = String(greenColorSaturationSlider.value)
+        blueColorSaturationLabel.text = String(blueColorSaturationSlider.value)
+    }
+
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        setCurrentColorViewColor()
+        redColorSaturationLabel.text = String(format: Constants.sliderValuerFormat,
+                                              redColorSaturationSlider.value)
+    }
+    
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        setCurrentColorViewColor()
+        greenColorSaturationLabel.text = String(format: Constants.sliderValuerFormat,
+                                                greenColorSaturationSlider.value)
+    }
+    
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        setCurrentColorViewColor()
+        blueColorSaturationLabel.text = String(format: Constants.sliderValuerFormat,
+                                               blueColorSaturationSlider.value)
+    }
+    
+    private func setCurrentColorViewColor() {
+        currentColorView.backgroundColor = UIColor(cgColor: CGColor(
+                                                    red: CGFloat(redColorSaturationSlider.value),
+                                                    green: CGFloat(greenColorSaturationSlider.value),
+                                                    blue: CGFloat(blueColorSaturationSlider.value),
+                                                    alpha: 1))
+    }
+}
+
+private extension UISlider {
+    
+    func defaultConfigure() {
+        self.minimumValue = 0.00
+        self.maximumValue = 1.00
+        self.value = 0.50
+    }
+}
+
+private extension UIView {
+    
+    func makeRound(with radius: CGFloat) {
+        self.layer.cornerRadius = radius
+    }
+}

25ca3a06316063539df248c39b43552d7325083b
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2021-10-27 10:30:40 +0300

    configure UI on LoginVC and add WelcomeVC

---
+++ 2.3/1/LoginSigueProject/LoginSigueProject/WelcomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  WelcomeViewController.swift
+//  LoginSigueProject
+//
+//  Created by Михаил Зиновьев on 27.10.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+}

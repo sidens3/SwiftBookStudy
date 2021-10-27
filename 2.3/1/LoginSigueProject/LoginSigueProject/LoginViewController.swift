c2d9c923cce8bd6726778c21531c22020f33ea9c
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2021-10-27 14:00:57 +0300

    create outlets and hide text in passwordTF

--- 2.3/1/LoginSigueProject/LoginSigueProject/LoginViewController.swift
+++ 2.3/1/LoginSigueProject/LoginSigueProject/LoginViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum Constants {
+    static let loginSegueIdentifier = "loginSegueIdentifier"
 }
 
+class LoginViewController: UIViewController {
+
+     
+    //MARK: - IBOutlet
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    //MARK: - Life cicle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        loginButton.layer.cornerRadius = 8
+    }
+    
+    //MARK: - IBAction
+    @IBAction func loginButtonPressed() {
+        let loginString = loginTextField.text ?? .empty
+        let passwordString = passwordTextField.text ?? .empty
+        if authorizationCheck(login: loginString, password: passwordString) {
+            self.performSegue(withIdentifier: Constants.loginSegueIdentifier, sender: self)
+        } else {
+            print("Ошибка авторизации")
+        }
+    }
+    
+    //MARK: - Privite
+    private func authorizationCheck(login: String, password: String) -> Bool {
+        //todo
+        // Тут выполняется авторизация, где false - неудачная автоизация, а true - удачная и проверка на пустые значения
+        return true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Constants.loginSegueIdentifier {
+            let vc = segue.destination as! WelcomeViewController
+            //todo
+            //setup env for segue
+        }
+    }
+}
+
+//MARK: - String
+extension String {
+    static let empty = ""
+}

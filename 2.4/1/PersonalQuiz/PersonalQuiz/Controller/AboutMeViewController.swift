62efe41011058ee05356ec57a9ca2c072848aad4
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2021-10-31 15:33:57 +0300

    add AboutMeViewController

---
+++ 2.4/1/PersonalQuiz/PersonalQuiz/Controller/AboutMeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutMeViewController.swift
+//  PersonalQuiz
+//
+//  Created by Михаил Зиновьев on 31.10.2021.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

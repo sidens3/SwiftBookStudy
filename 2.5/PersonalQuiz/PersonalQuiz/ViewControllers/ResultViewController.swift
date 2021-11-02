8c5882fa37e1d866e177a8a8bdacdc9ffd3dc105
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2021-11-02 19:24:52 +0300

    add folder for HW 2.5

---
+++ 2.5/PersonalQuiz/PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Nikita Zharinov on 01/11/2021.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результаты в соотвствии с этим животным
+    // 4. Избавиться от кнопки возврата назад на экране результатов
+    
+}
